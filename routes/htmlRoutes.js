f9a6f2b605c128f9a3f5d5b1ec9b18f8cca5271d
Jose Ferreira <jferreira590@gmail.com>
2019-08-10 12:26:49 -0400

    Started work on representatives call

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -7,10 +7,9 @@ module.exports = function(app) {
   app.get("/", function(req, res) {
     res.render("index");
   });
-
-  app.get("/results", function(req, res) {
+  app.get("/results", function(req, res, next) {
     var userAddress = req.query.userAddress;
-
+    // Get local candidates
     got(
       "https://www.googleapis.com/civicinfo/v2/voterinfo?address=" +
         userAddress +
@@ -27,17 +26,42 @@ module.exports = function(app) {
           election: body.election,
           apiKey: process.env.CIVIC_KEY
         };
-        //console.log(response.body.contests[0].candidates[0]);
         res.render("results", { results: results });
       })
       .catch(function(e) {
         console.error(e);
         res.redirect("/?error");
       });
+    next();
+  });
 
-    // Render 404 page for any unmatched routes
-    app.get("*", function(req, res) {
-      res.render("404");
-    });
+  app.get("/results", function(req, res) {
+    var userAddress = req.query.userAddress;
+    got(
+      "https://www.googleapis.com/civicinfo/v2/representatives?address=" +
+        userAddress +
+        "&key=" +
+        process.env.CIVIC_KEY,
+      { json: true }
+    )
+      .then(function(response) {
+        var body = response.body;
+        console.log("body");
+        var reps = {
+          repName: body.name,
+          imageUrl: body.photoUrl,
+          politicalParty: body.party,
+          apiKey: process.env.CIVIC_KEY
+        };
+        res.render("reps", { reps: reps });
+      })
+      .catch(function(e) {
+        console.error(e);
+      });
+  });
+
+  // Render 404 page for any unmatched routes
+  app.get("*", function(req, res) {
+    res.render("404");
   });
 };
